9373381925c3b4df842cb3fbb90cd4aa755d50ef
sakshi19104003 <113500014+sakshi19104003@users.noreply.github.com>
2022-11-26 23:14:23 +0530

    Add files via upload

---
+++ src/Assignment6/Question3/Calculator.java
@@ -0,0 +1,10 @@
+package Assignment6.Question3;
+
+public class Calculator {
+    Object first;
+    Object second;
+    public Calculator(Object a, Object b) {
+        this.first = a;
+        this.second = b;
+    }
+}
